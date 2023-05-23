b1ebb6898a12036e6b4162b68005e5f034f5c744
Admin <1036023528@qq.com>
2023-05-23 16:23:45 +0800

    项目初始化

---
+++ mall-coupoon/src/main/java/com/example/mall/coupoon/MallCoupoonApplication.java
@@ -0,0 +1,13 @@
+package com.example.mall.coupoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallCoupoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallCoupoonApplication.class, args);
+    }
+
+}
---
+++ mall-coupoon/src/test/java/com/example/mall/coupoon/MallCoupoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mall.coupoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCoupoonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
